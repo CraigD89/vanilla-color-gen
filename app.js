8d15b92ef734a36e17086c56faab65a0eff62e9c
Craig D <dupuiscraig@gmail.com>
2019-03-15 14:29:56 -0500

    adds hexadecimal generator

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-// alert("CONNECTED!");
-
 var body = document.getElementsByTagName("body");
-var button = document.getElementsByClassName("button");
+var button = document.getElementsByClassName("btn");
+var h1 = document.getElementsByTagName("h1");
+
+var hexGenerator = function(){
+    var length = 6;
+    var chars = '0123456789ABCDEF';
+    var hex = '#';
+    while(length--) hex += chars[(Math.random() * 16) | 0];
+    return hex;
+};
+// console.log(hexGenerator());
