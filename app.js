133707873e4570ffbac63ac9467d1896f8a1d31e
Craig D <dupuiscraig@gmail.com>
2019-03-16 11:03:21 -0500

    refactors hex generator and sets up button functionality

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ var body = document.querySelector("body");
 var button = document.querySelector(".btn");
 var h1 = document.querySelector("h1");
 
-console.log(body);
-console.log(button);
-console.log(h1);
+// console.log(body);
+// console.log(button);
+// console.log(h1);
+
+// var hexGenerator = function() {
+//   var length = 6;
+//   var chars = "0123456789ABCDEF";
+//   var hex = "#";
+//   while (length--) hex += chars[(Math.random() * 16) | 0];
+//   return hex;
+// };
 
 var hexGenerator = function() {
-  var length = 6;
-  var chars = "0123456789ABCDEF";
-  var hex = "#";
-  while (length--) hex += chars[(Math.random() * 16) | 0];
-  return hex;
+  var letters = "0123456789ABCDEF";
+  var color = "";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16) | 0];
+  };
+  return "#" + color;
 };
 
 console.log(hexGenerator());
@@ -21,3 +30,8 @@ console.log(hexGenerator());
 // Values are not curently matching
 body.style.backgroundColor = hexGenerator();
 h1.innerText = hexGenerator();
+
+// Setting up button click to trigger random color
+button.addEventListener('click', function(){
+  console.log(hexGenerator());
+});
